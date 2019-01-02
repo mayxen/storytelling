7b715534813c042a6bb28ee3c4654755475270aa
ivan Curbelo <ivancurbelotrabajo@gmail.com>
2019-01-02 10:53:02 +0000

    cambio para presentacion1

---
+++ Assets/proyect/script/ballMov.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballMov : MonoBehaviour
+{
+    public int speed;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector3(Input.GetAxis("Horizontal")*speed,rb.velocity.y, Input.GetAxis("Vertical") * speed);
+    }
+}
